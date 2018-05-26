4c87bf060c8a75da6798d2ed12251635ba07359d
Jeancarlos De la Cruz Criollo <jdelacruz2712@gmail.com>
2018-05-26 12:04:28 -0500

    Se empieza a trabajar en las rutas de la app

---
+++ src/components/promociones.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <h1>
+      Promociones
+    </h1>
+  </section>
+</template>
+
+<script>
+    export default {
+        name: "promociones"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/saldos_movimientos.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <div class="title">
+      <h1>
+        Saldos y Movimientos
+      </h1>
+    </div>
+  </section>
+</template>
+
+<script>
+    export default {
+        name: "saldos_movimientos",
+    }
+</script>
+
+<style scoped>
+
+</style>
